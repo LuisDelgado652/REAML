89b6d9ca87c18c5f71cd0ac6bd9ca718635544a7
LuisDelgado652 <luisdelgadobravo652@gmail.com>
2021-12-17 00:53:04 -0500

    realm new

---
+++ app/src/main/java/com/luisdelgado/reaml/TaskStatus.java
@@ -0,0 +1,11 @@
+package com.luisdelgado.reaml;
+
+public enum TaskStatus {
+    Open("Open"),
+    InProgress("In Progress"),
+    Complete("Complete");
+    String displayName;
+    TaskStatus(String displayName) {
+        this.displayName = displayName;
+    }
+}
